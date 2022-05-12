fb92ea9061fcba6701e13ff7e3994a7d3a70014d
Daniel Oy <daniel-oy-mis@hotmail.com>
2022-05-11 21:58:18 -0500

    chore: UI project added (Mvc)

---
+++ LeaveManagement.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagement.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
